b4b3e613f2bfea0e3792d867980d569d81938bce
Océane GREGOIRE <119354223+ogregoir@users.noreply.github.com>
2024-10-03 11:47:03 +0200

    ex02

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ogregoir <ogregoir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 15:57:22 by ogregoir          #+#    #+#             */
-/*   Updated: 2024/09/13 13:51:10 by marvin           ###   ########.fr       */
+/*   Updated: 2024/09/16 15:11:54 by ogregoir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,12 @@ int main(int argc, char **argv)
         {
             btc.parse_input(argv);
         }
-        catch
-        {
-            
-        }
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
     }  
     else
         std::cerr << "Error: could not open file." << std::endl;
+    return 0;
 }   
